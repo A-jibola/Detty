2658969d7444188463aec24a235d3b332ca50b83
A-jibola <73576882+A-jibola@users.noreply.github.com>
2024-01-21 22:40:52 +0100

    Update Footer.js

--- view/src/Components/Footer.js
+++ view/src/Components/Footer.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Footer({style}) {
+
+    const [category, SetCategory] = useState('')
+    const [name, setName] = useState('')
+  
   return (
     <div className={style}>
         <div className='row p-4'>
             <div className='col-lg-6 col-md-9 mb-3'>
                 <h3 className='bold italic pb-0 mb-0'>Detty</h3>
                 <p className='mt-0 mb-2'>@Copyright 2024</p>
-                <input type="text" name="fname" />
-                <select id="cars" name="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    <option value="audi">Audi</option>
-                </select>
-                <button className="button button-dark"> Search </button>
+                <input type="text" onChange={e=>setName(e.target.value)} name="fname" />
+                 <select id="services" onChange={e=> SetCategory(e.target.value)} name="services">
+                        <option>All</option>
+                        <option value="Shortlets">Shortlets</option>
+                        <option value="Ride Service">Ride Service</option>
+                        <option value="Resturants">Resturants</option>
+                        <option value="Events">Events</option>
+                        <option value="Others">Others</option>
+                  </select>
+                <a href={`/search/${category}/${name}`} className="button button-dark"> Search </a>
             </div>
             <div className='col-lg-4 col-md-9'>
                 <h3 className='bold italic'>Contact Us</h3>
